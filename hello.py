87ec67c6f67b66c292942e6952be6e0c8a27661f
Benson Ndiwa <bnkimtai@gmail.com>
2022-12-06 13:56:41 +0300

    Loop in templates

--- hello.py
+++ hello.py
@@ -49,6 +49,14 @@ def login():
 def hello_name(user):
    return render_template("hello.html", name = user)
 
+@app.route('/marks/<int:score>')
+def marks(score):
+   return render_template('marks.html', marks = score)
+
+@app.route('/result')
+def result():
+   dict = {'phy':50,'che':60, 'maths':70}
+   return render_template('result.html', result = dict)
 def hi_ben():
    return "Sasa Ben"
 
